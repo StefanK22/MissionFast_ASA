3945c00cebcf94b7aad06f1c032ab03f002e020b
Tomas Nascimento <gomes.nascimento@tecnico.ulisboa.pt>
2021-12-10 19:34:43 +0000

    Fix algorithm1

--- firstTry.cpp
+++ firstTry.cpp
@@ -24,17 +24,6 @@ int max_element(vector<int> list){
 	return max;
 }
 
-int howMany(vector<int> list, int value){
-	int i=0,total=0;
-	while(i<list.size()){
-		if(list[i]==value){
-			total++;
-		}
-		i++;
-	}
-	return total;
-}
-
 int getNumberOfsub(vector<int> list, vector<int> len ,int value){
 	int i = 0, index, total = 0;
 	while((index = getindex(list, value, i)) != -1){
@@ -56,7 +45,7 @@ int algoritmo1(vector<int> list){
 		for (j = 0; j < i - 1; j++){
 			if (list[i] > list[j]){
 				if (newList1[j] + 1 > newList1[i]){
-					newList1[i] = newList[j] + 1;
+					newList1[i] = newList1[j] + 1;
 					newList2[i] = newList2[j];
 				} else if (newList1[j] + 1 == newList1[i])
 					newList2[i] += newList2[j];
@@ -70,6 +59,10 @@ int algoritmo1(vector<int> list){
 	for (i = 0; i < n; i++){
 		printf("%d\n", newList2[i]);
 	}
+
+	int value = max_element(newList1);
+	int numOflists = getNumberOfsub(newList1 , newList2, value);
+	printf("%d %d\n", value , numOflists);
 	return 0;
 }
 
