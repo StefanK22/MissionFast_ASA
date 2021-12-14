d013c0edf34e70d6bb118bb1dbc56186cdc97db9
Tomas Nascimento <gomes.nascimento@tecnico.ulisboa.pt>
2021-12-14 18:16:49 +0000

    Add algoritm2

--- firstTry.cpp
+++ firstTry.cpp
@@ -4,38 +4,9 @@
 
 using namespace std;
 
-int getindex(vector<int> list, int value,int i){
-	while(i<list.size()){
-		if(list[i]==value)
-			return i;
-		i++;
-	}
-	return -1;
-}
-
-int max_element(vector<int> list){
-	int i=0,max=0;
-	while(i<list.size()){
-		if(list[i]>max){
-			max = list[i];
-		}
-		i++;
-	}
-	return max;
-}
-
-int getNumberOfsub(vector<int> list, vector<int> len ,int value){
-	int i = 0, index, total = 0;
-	while((index = getindex(list, value, i)) != -1){
-		total = total + len[index];
-		i = index + 1;
-	}
-	return total;
-}
-
 
 int algoritmo1(vector<int> list){
-	int i, j, n = list.size();
+	int i, j, value = 1, numOflists = 0,n = list.size();
 	vector<int> newList1, newList2;
 	newList1.resize(n);
 	newList2.resize(n);
@@ -50,6 +21,10 @@ int algoritmo1(vector<int> list){
 			if (list[i] > list[j]){
 				if (newList1[j] + 1 > newList1[i]){
 					newList1[i] = newList1[j] + 1;
+					if(newList1[i] > value){
+						value = newList1[i];
+						numOflists = i;
+					}
 					newList2[i] = newList2[j];
 				} else if ((newList1[j] + 1) == newList1[i])
 					newList2[i] += newList2[j];
@@ -65,9 +40,43 @@ int algoritmo1(vector<int> list){
 		printf("%d\n", newList2[i]);
 	}
 
-	int value = max_element(newList1);
-	int numOflists = getNumberOfsub(newList1 , newList2, value);
-	printf("%d %d\n", value , numOflists);
+	
+	printf("%d %d\n", value , newList2[numOflists]);
+	return 0;
+}
+
+
+int algoritmo2(vector<int> list1, vector<int> list2){
+	int i, j, total = 0;
+	int size1 = list1.size();
+	int size2 = list2.size();
+	vector<int> newList;
+	if(size1 > size2)
+		newList.resize(size1);
+	else
+		newList.resize(size2);
+
+	for(i = 0; i < newList.size(); i++){
+		newList[i] = 0;
+	}
+
+	for(j=0; i<size1; i++){
+		int currSize = 0;
+
+		for(i=0; j<size2; j++){
+
+			if (list1[i] == list2[j] && currSize + 1 > newList[j])
+                newList[j] = currSize + 1;
+ 
+            if (list1[i] > list2[j] && newList[j] > currSize){
+            	currSize = newList[j];
+            	if(currSize > total)
+            		total = currSize;
+            }
+		}
+	}
+
+	printf("%d\n",total);
 	return 0;
 }
 
